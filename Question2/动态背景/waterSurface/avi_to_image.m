b2f32c2feacd97d6ed1c6c240427412f53a4f4a3
Chen Bing <1252242281@qq.com>
2018-09-09 13:07:15 +0800

    Add files via upload

---
+++ Question2/动态背景/waterSurface/avi_to_image.m
@@ -0,0 +1,16 @@
+%将视频转换为单张图片
+clear
+clc
+
+file_name = 'mask.avi';        %视频所在文件夹
+obj = VideoReader(file_name);     %读取视频文件
+
+numFrames = obj.NumberOfFrames;   %视频总的帧数 
+for k = 1: numFrames
+    frame = read(obj,k);
+    %imshow(frame);                
+    gray_frame = rgb2gray(frame); %若每一帧为彩色图片，转换为灰度图
+    imshow(frame);                %显示每一帧图片
+    %保存每一帧图片
+    imwrite(gray_frame,strcat('D:\数学建模代码\2017\D\附件2-典型视频\不带晃动-动态背景\waterSurface\',num2str(k),'.jpg'),'jpg');
+end
---
+++ Question2/动态背景/waterSurface/result/Review.m
@@ -0,0 +1,43 @@
+%%Time: 2017.9.19
+%%Name:Michael Beechan(陈兵)
+%%School:Chongqing university of technology
+%%Function: Review my Algorithm
+function Review
+img0 = imread('1.jpg');
+[rows, cols] = size(img0);
+img_num = 30;
+TP = 0; %正确目标点数
+FP = 0; %错误目标的点数
+FN = 0; %错误背景点数
+TN = 0; %正确背景点数
+for num = 21 : img_num
+    temp1 = im2bw(imread(strcat(num2str(num),'.jpg'),'jpg'));
+    temp2 = im2bw(imread(strcat(num2str(num + 18),'.jpg'),'jpg'));
+    for i = 1 : rows
+        for j = 1 : cols
+           if temp1(i,j) == 1 && temp2(i,j) == 1 
+               TP = TP + 1;
+           elseif temp1(i,j) == 1 && temp2(i,j) == 0 
+               FP = FP + 1;
+           elseif temp1(i,j) == 0 && temp2(i,j) == 1 
+               FN = FN + 1;
+           else temp1(i,j) == 0 && temp2(i,j) == 0;
+               TN = TN + 1;
+           end
+        end
+    end
+end
+Re = TP / (TP + FN) %Recall
+Sp = TN / (TN + FP) %Specificity
+FPR = FP / (FP + FN) %False Positive Rate
+FNR = FN / (TP + FN) %False Negative Rate
+PWC = 100 * (FN + FP) / (TP + FN + FP + TN) %Percentage of Wrong Classification
+Precision = TP / (TP + FP)
+F_Measure = (2 * Precision * Re) / (Precision + Re)
+
+
+
+
+
+
+
